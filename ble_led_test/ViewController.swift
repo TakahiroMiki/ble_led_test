5caf8faeca49f59227913b5c6c5d1b589b10679c
tmtakahiro <tmtakahiro@gmail.com>
2016-10-13 23:41:54 +0900

    Initial Commit

---
+++ ble_led_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ble_led_test
+//
+//  Created by 三木隆裕 on 2016/10/13.
+//  Copyright © 2016年 tmtakahiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
